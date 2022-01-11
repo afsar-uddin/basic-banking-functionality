6d0058ce4fa06f4570d698f2a798b9c91e511d85
Afsar Uddin <afsarwebdev@gmail.com>
2022-01-11 13:24:58 +0600

    validation

--- scripts.js
+++ scripts.js
@@ -34,15 +34,19 @@ function updateBalance(amount, isAdd) {
 
 // Deposit
 document.getElementById('deposit-btn').addEventListener('click', function () {
-
     const newDepositAmount = getInputValue('deposit-input');
+    // console.log(newDepositAmount)
 
     if (newDepositAmount > 0) {
         updateTotalField('deposit-total', newDepositAmount);
-
         updateBalance(newDepositAmount, true);
     }
-
+    if (newDepositAmount !== '') {
+        document.getElementById('deposit-warning').classList.add('dnone');
+        setTimeout(() => {
+            document.getElementById('deposit-warning').classList.remove('dnone');
+        }, 2000);
+    }
 
 });
 
@@ -57,7 +61,16 @@ document.getElementById('withdraw-btn').addEventListener('click', function () {
         updateBalance(withdrawAmount, false);
     }
     if (withdrawAmount > currentBalance) {
-        console.log('You can not withdraw more thn what you in your account')
+        document.getElementById('withdraw-warning').classList.add('dnone');
+        setTimeout(() => {
+            document.getElementById('withdraw-warning').classList.remove('dnone');
+        }, 2000);
+    } else if (withdrawAmount < 0 || withdrawAmount !== '') {
+        document.getElementById('withdraw-warning-0').classList.add('dnone');
+        setTimeout(() => {
+            document.getElementById('withdraw-warning-0').classList.remove('dnone');
+        }, 2000);
     }
+
 
 })
